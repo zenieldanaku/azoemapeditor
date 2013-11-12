75f75bd4002e3b57f950af6433c31526d1615ba9
daniel.e.rossy@gmail.com <daniel.e.rossy@gmail.com@226ea508-57a5-042f-a199-f4aa06a49ad1>
2013-11-12 20:20:34 +0000

    Ahora se pueden mover las barras de desplazamiento. Aun no está implementado el drag and drop, pero no falta mucho. Falta también modularizar.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ C = 32
 tamanio = 24*C,20*C
 pantalla.set_caption("MapGen")
 fondo = pantalla.set_mode(tamanio)
+
 gris = pygame.Color(125,125,125)
 grilla_rect = pygame.Rect(((2*C)+2,2*C),(16*C,16*C))
 herramientas = pygame.Rect((0,2*C),(2*C,16*C))
@@ -18,7 +19,19 @@ estado = pygame.Rect((0,(19*C)-2),(24*C,1*C))
 menu_1 = pygame.Rect((0,0),(24*C,1*C))
 menu_2 = pygame.Rect((0,C),(24*C,1*C))
 
+c_H_press = False
+c_V_press = False
+
+FPS = pygame.time.Clock()
+
+fondo.fill(gris)
+pygame.draw.rect(fondo, (255,255,255), barra_V)
+pygame.draw.rect(fondo, (255,255,255), barra_H)
+pygame.draw.rect(fondo, (125,0,125),cursor_H)
+pygame.draw.rect(fondo, (125,0,125),cursor_V)
+
 while True:
+    FPS.tick(60)
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
@@ -28,15 +41,41 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-    fondo.fill(gris)
+                
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                x,y = pygame.mouse.get_pos()
+                x = int(x/32)*C+5
+                y = int(y/32)*C+5
+                if cursor_H.collidepoint(x,y): 
+                    c_H_press = True
+                elif cursor_V.collidepoint(x,y):
+                    c_V_press = True
+        
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                x,y = pygame.mouse.get_pos()
+                x = int(x/32)*C+5
+                y = int(y/32)*C+5
+                if barra_V.collidepoint(x,y):
+                    if c_V_press:
+                        pygame.draw.rect(fondo, (255,255,255), barra_V)
+                        _cursor_V = pygame.Rect((x,y),cursor_V.size)
+                        fondo.fill((125,0,125),(_cursor_V))
+                        cursor_V = _cursor_V
+                elif barra_H.collidepoint(x,y):
+                    if c_H_press:
+                        pygame.draw.rect(fondo, (255,255,255), barra_H)
+                        _cursor_H = pygame.Rect((x,y),cursor_H.size)
+                        fondo.fill((125,0,125),((x,y),cursor_H.size))
+                        fondo.fill((125,0,125),(_cursor_H))
+                        cursor_H = _cursor_H
+                
+        
     pygame.draw.rect(fondo, (0,0,0), grilla_rect, 2)
     pygame.draw.rect(fondo, (0,0,0), herramientas, 2)
     pygame.draw.rect(fondo, (0,0,0), panel, 2)
-    pygame.draw.rect(fondo, (255,255,255), barra_V)
-    pygame.draw.rect(fondo, (255,255,255), barra_H)
     pygame.draw.rect(fondo, (0,0,0), estado, 2)
-    pygame.draw.rect(fondo, (125,0,125),cursor_H)
-    pygame.draw.rect(fondo, (125,0,125),cursor_V)
     pygame.draw.rect(fondo,(0,0,0),menu_1,2)
     pygame.draw.rect(fondo,(0,0,0),menu_2,2)
-    pantalla.update()
+    pantalla.flip()
