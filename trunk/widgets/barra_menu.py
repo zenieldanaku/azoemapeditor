2da72f5e1e9c3c09e446c96de7d53b18fe248e58
daniel.e.rossy@gmail.com <daniel.e.rossy@gmail.com@226ea508-57a5-042f-a199-f4aa06a49ad1>
2013-11-15 18:44:32 +0000

    Menus: * Añadido el package libs que contiene la función render_textrect. * Añadidos tres botones falsos (aún no hace nada) a menú 1. * Nueva clase boton_menu.
    * la función addWidget del Renderer ahora añade los widgets al layer 1 por defecto, en lugar del layer 0.
    
    new issue
    Summary: focus doble
    Labels: Priority-Low, Type-Defect
    Añadí un print al mainloop para ver qué widget ganaba el foco, y noté que tanto el widget target como la ventana (o lo que esté debajo) gana también foco. Me parece que eso es un error, y tiene que ver con el loop que hacemos sobre render_engine.contents.

--- trunk/widgets/barra_menu.py
+++ trunk/widgets/barra_menu.py
@@ -1,12 +1,41 @@
 from .basewidget import BaseWidget
 from constantes import *
-from pygame import Rect,Surface,draw
+from pygame import Rect,Surface,draw,font
+from pygame.sprite import LayeredDirty
+from libs.textrect import render_textrect
+from renderer import render_engine
+
 
 class Menu_barra(BaseWidget):
-    def __init__(self,nom_menu,x,y,w,h):
+    botones = None
+    
+    def __init__(self,nom_menu,x,y,w,h,botones):
         super().__init__()
+        self.botones = LayeredDirty()
         self.nombre = nom_menu
         self.rect = Rect(x,y,w,h)
         self.image = Surface(self.rect.size)
         self.image.fill(gris)
         draw.rect(self.image,negro,(0,0,w-2,h-2),2)
+    
+        for b in range(len(botones)):
+            boton = boton_menu(botones[b])
+            boton.rect.topleft = ((boton.image.get_width()+20)*b)+4,6
+            self.botones.add(boton)
+            render_engine.addWidget(boton,2)
+        
+        self.botones.draw(self.image)
+            
+class boton_menu(BaseWidget):
+    nombre = ''
+    
+    def __init__(self,nombre):
+        super().__init__()
+        self.nombre = nombre
+        self.layer = 2
+        fuente = font.SysFont('Verdana',14)
+        w,h = fuente.size(nombre)
+        rect = Rect(-1,-1,w,h+1)
+        render = render_textrect(nombre,fuente,rect,negro,gris,1)
+        self.image = render
+        self.rect = render.get_rect()
