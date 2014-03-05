940b8e3bb8628b9d8bb14399e692c6f7ed7d2ff4
moguchicorp@gmail.com <moguchicorp@gmail.com@226ea508-57a5-042f-a199-f4aa06a49ad1>
2014-03-05 04:58:28 +0000

    botones biselados

--- widgets/boton.py
+++ widgets/boton.py
@@ -10,20 +10,26 @@ class Boton(BaseWidget):
         self.x,self.y = x,y
         self.nombre = nombre
         self.comando = cmd
-        self.img_uns = self._crear(texto,negro,negro)
-        self.img_sel = self._crear(texto,cian_claro,negro)
-        self.img_pre = self._crear(texto,cian_claro,blanco)
+        c1 = Color(100,100,100)
+        c2 = Color(150,150,150)
+        
+        self.img_uns = self._crear(texto,negro,gris, c1, c2)
+        self.img_sel = self._crear(texto,cian_claro,gris, c1, c2)
+        self.img_pre = self._crear(texto,cian_claro,gris, c2, c1)
+        
+        
         self.descripcion = descripcion
         
         self.image = self.img_uns
         self.rect = self.image.get_rect(topleft=(x,y))
     
-    def  _crear(self,texto,color,color_marco):
+    def  _crear(self, texto, color_texto, color_fondo, color_marco1, color_marco2):
         fuente = font.SysFont('verdana',16)
         rect = Rect(0,0,28,25)
-        render = render_textrect(texto,fuente,rect,color,gris,1)
-        marco = Rect(rect.topleft,(rect.w-2,rect.h-2))
-        draw.rect(render,color_marco,marco,2)
+        render = render_textrect(texto,fuente,rect,color_texto,color_fondo,1)
+        draw.line(render, color_marco1, (0,rect.h-3),(rect.w-3,rect.h-3), 2)
+        draw.line(render, color_marco1, (rect.w-3,rect.h-2),(rect.w-3,0), 2)
+        draw.lines(render, color_marco2, False, [(rect.w-2,0),(0,0),(0,rect.h-4)], 2)
         return render
     
     def serElegido(self):
